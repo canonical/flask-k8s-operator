ce77e1d81bf07b743ef73f1c65cbd11c24f16b1e
Weii Wang <weii.wang@canonical.com>
2023-04-12 11:26:02 +0800

    Refactor the integration test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -14,31 +14,32 @@ from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
 
-APP_NAME = "flask-k8s"
 
 
-@pytest_asyncio.fixture
-async def build_and_deploy(ops_test: OpsTest, pytestconfig: pytest.Config):
+
+@pytest_asyncio.fixture(scope="module", name="flask_app")
+async def flask_app_fixture(ops_test: OpsTest, pytestconfig: pytest.Config):
     """Build and deploy the flask charm."""
     assert ops_test.model
+    app_name = "flask-k8s"
     charm = await ops_test.build_charm(".")
     resources = {"flask-app-image": pytestconfig.getoption("--flask-app-image")}
-    await asyncio.gather(
+    deploy_result = await asyncio.gather(
         ops_test.model.deploy(
-            charm, resources=resources, application_name=APP_NAME, series="jammy"
+            charm, resources=resources, application_name=app_name, series="jammy"
         ),
-        ops_test.model.wait_for_idle(apps=[APP_NAME], raise_on_blocked=True),
+        ops_test.model.wait_for_idle(apps=[app_name], raise_on_blocked=True),
     )
+    return deploy_result[0]
 
 
-@pytest.mark.usefixtures("build_and_deploy")
-async def test_flask_is_up(get_unit_ips):
+async def test_flask_is_up(flask_app, get_unit_ips):
     """
     arrange: build and deploy the flask charm.
     act: send a request to the flask application managed by the flask charm.
     assert: the flask application should return a correct response.
     """
-    for unit_ip in await get_unit_ips(APP_NAME):
+    for unit_ip in await get_unit_ips(flask_app.name):
         response = requests.get(f"http://{unit_ip}:8000", timeout=5)
         assert response.status_code == 200
         assert "Hello, World!" in response.text
