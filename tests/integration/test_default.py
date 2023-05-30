91d283b7e5f7ff550472fe2a3e2d4a94d1054b3a
Weii Wang <weii.wang@canonical.com>
2023-05-30 19:50:37 +0800

    Add a default flask application image

---
+++ tests/integration/test_default.py
@@ -0,0 +1,39 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Integration tests for flask-k8s charm sentinel check."""
+
+import typing
+
+import requests
+from juju.model import Model
+from pytest import Config
+from pytest_operator.plugin import OpsTest
+
+
+async def test_sentinel_check(
+    pytestconfig: Config,
+    ops_test: OpsTest,
+    model: Model,
+        get_unit_ips: typing.Callable[[str], typing.Awaitable[tuple[str, ...]]],
+
+):
+    """
+    arrange: none.
+    act: build and deploy the flask-k8s charm with default image as the flask-app-image.
+    assert: flask-k8s charm should run the default Flask application.
+    """
+    charm = await ops_test.build_charm(".")
+    resources = {
+        "flask-app-image": pytestconfig.getoption("--flask-app-image"),
+        "statsd-prometheus-exporter-image": "prom/statsd-exporter",
+    }
+    app_name = "flask-sentinel"
+    await model.deploy(
+        charm, resources=resources, application_name=app_name, series="jammy"
+    )
+    await model.wait_for_idle(apps=[app_name], raise_on_blocked=True)
+    for unit_ip in await get_unit_ips(app_name):
+        resp = requests.get(f"http://{unit_ip}:8000", timeout=10)
+        assert resp.ok
+        assert "Welcome to flask-k8s Charm" in resp.text
