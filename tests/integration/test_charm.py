ecda92f7b3634f2330e57c1209465ca2687dfee6
Niels Robin-Aubertin <git@niels.fr>
2023-05-30 10:25:15 +0200

    Add trust arg for db integ tests

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -191,10 +191,10 @@ async def test_with_ingress(
 
 
 @pytest.mark.parametrize(
-    "endpoint,db_name, db_channel",
+    "endpoint,db_name, db_channel, trust",
     [
-        ("mysql/status", "mysql-k8s", "8.0/stable"),
-        ("postgresql/status", "postgresql-k8s", "14/stable"),
+        ("mysql/status", "mysql-k8s", "8.0/stable", True),
+        ("postgresql/status", "postgresql-k8s", "14/stable", False),
     ],
 )
 async def test_with_database(
@@ -204,13 +204,18 @@ async def test_with_database(
     endpoint: str,
     db_name: str,
     db_channel: str,
+    trust: bool,
 ):
     """
     arrange: build and deploy the flask charm.
     act: deploy the database and relate it to the charm.
     assert: requesting the charm should return a correct response
     """
-    db_app = await model.deploy(db_name, channel=db_channel)
+    db_app = await model.deploy(
+        db_name,
+        channel=db_channel,
+        trust=trust
+    )
     await model.wait_for_idle()
 
     await model.add_relation(flask_app.name, db_app.name)
