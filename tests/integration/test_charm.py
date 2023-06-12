b4393371a4b13cd0e058a6fa4c0fae02c80a2fb0
Weii Wang <weii.wang@canonical.com>
2023-06-12 18:10:57 +0800

    Fix a configuration name error in integration test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -95,7 +95,7 @@ async def test_default_secret_key(
         ),
         pytest.param({"flask_debug": True}, {"DEBUG": True}, id="debug"),
         pytest.param(
-            {"flask_secret_key": "foobar"}, {"FLASK_SECRET_KEY": "foobar"}, id="secret_key"
+            {"flask_secret_key": "foobar"}, {"SECRET_KEY": "foobar"}, id="secret_key"
         ),
     ],
     indirect=["update_config"],
